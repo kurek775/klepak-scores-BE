decd173c805ea14d368851493f62f5e38616168f
kurek775 <pavelkurek@seznam.cz>
2024-11-21 19:10:57 +0100

    WIP

--- models/template.py
+++ models/template.py
@@ -4,3 +4,14 @@ from sqlalchemy import (
 from sqlalchemy.orm import relationship, declarative_base
 
 Base = declarative_base()
+
+class Template(Base):
+    __tablename__ = "templates"
+
+    id = Column(Integer, primary_key=True)
+    bgImage = Column(LargeBinary)
+    font = Column(LargeBinary)
+    textPosition = Column(String)
+
+    # Relationships
+    tours = relationship("Tour", back_populates="template")
