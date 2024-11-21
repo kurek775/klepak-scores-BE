3141ec85f5ad556d0b47163a79d8909434ddfee1
kurek775 <pavelkurek@seznam.cz>
2024-11-21 19:43:36 +0100

    WIP - REST API

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ## 1 python -m venv venv
 ## 2 venv\Scripts\activate
 ## 3 pip install fastapi[all]
-## 4 uvicorn main:app
+## 4 uvicorn main:app --reload
 
 ## tutorial : https://www.youtube.com/watch?v=0sOvCWFmrtA&ab_channel=freeCodeCamp.org
--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
-app = FastAPI()
+app = FastAPI(openapi_url="/api/openapi.json", docs_url="/api/docs", redoc_url="/api/redoc")
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
+api_router = APIRouter(prefix="/api")
 
-@app.get("/api/")
-def root():
-    return {"message": "Hello API"}
+# Define the routes under the "/api" prefix
+@api_router.post("/tours/{tourId}/crews/{crewId}/upload-photo")
+def upload_file(tourId: int, crewId: int):
+    return {"message": f"Photo uploaded for tour {tourId} and crew {crewId}"}
+
+@api_router.post("/tours/{tourId}/crews/{crewId}/sports/{sportId}/results")
+def create_results(tourId: int, crewId: int,sportId: int):
+    return {"message": "Results created"}
+
+@api_router.get("/results")
+def get_results():
+    return {"data": "data"}
+
+@api_router.put("/results")
+def update_results():
+    return {"data": "data"}
+
+@api_router.get("/crews")
+def get_crew():
+    return {"data": "data"}
+
+@api_router.get("/sports")
+def get_sports():
+    return {"data": "data"}
+
+# Include the router in the main app
+app.include_router(api_router)
